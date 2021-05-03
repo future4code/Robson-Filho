c798d3da06e47f672265d85c4774900d9bb672bc
robinndev <robson_dev@hotmail.com>
2021-05-03 16:51:41 -0300

    inicio do exercicio de js

---
+++ template_javascript/index.js
@@ -0,0 +1,10 @@
+const name =;
+const idade; 
+
+typeof(name, idade)
+
+//Deu algo como "vazia", creio que seja pelo fato de nao ter lhe dado valor
+
+window.prompt("ola mundo, qual o seu nome?")
+window.prompt("ola mundo, qual a sua idade?")
+window.alert("Ola mundo, oi" name "como voce esta? vi que voce tem" idade "de idade")
