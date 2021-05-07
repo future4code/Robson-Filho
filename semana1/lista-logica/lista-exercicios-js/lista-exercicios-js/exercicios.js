1bdd66a2e5933f98c0d572ddc3b86260646f3c39
Robinndev <robson_dev@hotmail.com>
2021-05-07 12:31:59 -0300

    Enviando o exercicio 1 de 15

---
+++ semana1/lista-logica/lista-exercicios-js/lista-exercicios-js/exercicios.js
@@ -0,0 +1,117 @@
+// Exemplos
+
+// Exercício 0A
+
+function soma(num1, num2) {
+   // implemente sua lógica aqui
+   return num1 + num2
+}
+
+// Exercício 0B
+
+function imprimeMensagem() {
+   // implemente sua lógica aqui
+   const mensagem = prompt('Digite uma mensagem!')
+
+   console.log(mensagem)
+}
+
+// Exercícios
+
+//Exercício 1
+
+function calculaAreaRetangulo() {
+   // implemente sua lógica aqui
+   const num1 = Number(prompt('Qual a Base do seu retangulo?'))
+   const num2 = Number(prompt('Qual a altura do seu triangulo?'))
+   console.log(num1 * num2)
+  
+}
+
+
+
+//Exercício 2
+
+function imprimeIdade() {
+   // implemente sua lógica aqui
+}
+
+//Exercício 3
+
+function calculaIMC(peso, altura) {
+   // implemente sua lógica aqui
+
+}
+
+//Exercício 4
+
+function imprimeInformacoesUsuario() {
+   // implemente sua lógica aqui
+
+}
+
+//Exercício 5
+
+function imprimeTresCoresFavoritas() {
+   // implemente sua lógica aqui
+}
+
+//Exercício 6
+
+function retornaStringEmMaiuscula(string) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 7
+
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 8
+
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+}
+
+// Exercício 9
+
+function retornaPrimeiroElemento(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 10
+
+function retornaUltimoElemento(array) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 11
+
+function trocaPrimeiroEUltimo(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 12
+
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 13
+
+function checaRenovacaoRG() {
+   // implemente sua lógica aqui
+}
+
+// Exercício 14
+
+function checaAnoBissexto(ano) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 15
+
+function checaValidadeInscricaoLabenu(pessoa) {
+   // implemente sua lógica aqui
+}
