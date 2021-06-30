bae7b95e951c2ab2694a8a850f37f08dd13fbfc5
Robinndev <robson_dev@hotmail.com>
2021-06-30 19:03:36 -0300

    ADICIONANDO ITENS

---
+++ semana9/astromatch/astromatch-app/src/api/astromatch.jsx
@@ -0,0 +1,3 @@
+const function url() {'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/person'}
+
+export default url
--- semana9/astromatch/astromatch-app/src/components/telas/inicio/TelaInicio.jsx
+++ semana9/astromatch/astromatch-app/src/components/telas/inicio/TelaInicio.jsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-
+import url from '../../../api/astromatch'
 
 
 export const TelaInicio = () => {
 
-    const [id, setId] = useState(0)
+    const [id, setId] = useState([])
     const [age, setAge] = useState('')
-    const [name, setName] = ('')
+    const [name, setName] = useState('')
     const [photo, setPhoto] = useState('')
     const [bio, useState] = useState('')
-    
+
+    useEffect(() => {
+        url()
+    }, [])
+
     return (
     <div>
         <h1>
