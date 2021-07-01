0c2c9da5d71e540b91d513baeaa4a73b18b71169
Robinndev <robson_dev@hotmail.com>
2021-07-01 20:34:15 -0300

    criando funcionalidades deslike

--- semana9/astromatch/astromatch-app/src/api/astromatch.jsx
+++
@@ -1,3 +0,0 @@
-const function url() {'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/person'}
-
-export default url
--- semana9/astromatch/astromatch-app/src/components/telas/inicio/TelaInicio.jsx
+++ semana9/astromatch/astromatch-app/src/components/telas/inicio/TelaInicio.jsx
@@ -1,25 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import url from '../../../api/astromatch'
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { object } from "prop-types";
 
 export const TelaInicio = () => {
+  const [profile, setProfile] = useState("");
+  const [people, setPeople] = useState({})
 
-    const [id, setId] = useState([])
-    const [age, setAge] = useState('')
-    const [name, setName] = useState('')
-    const [photo, setPhoto] = useState('')
-    const [bio, useState] = useState('')
+  const [newLoad, setNewLoad] = useState(1)
 
-    useEffect(() => {
-        url()
-    }, [])
-
-    return (
-    <div>
-        <h1>
-            INICIO
-        </h1>
-    </div>
+  const getProfile = () => {
+    axios
+    .get(
+      `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:${profile}/person`
     )
-}
+    .then((res) => {
+      setPeople(res.data.profile);
+    })
+    .catch((err) => {
+      console.log("Por gentileza, tentar novamente!");
+    });
+  }
+
+  useEffect(() => {
+ 
+    getProfile()
+  }, [newLoad]);
+
+  const like = (event) => {
+    setProfile(event.target.value);
+  };
+
+  const deslike = () => {
+    getProfile()
+    setNewLoad(newLoad +1)
+    console.log(newLoad)
+    console.log(people)
+  };
+
+  return (
+    <div>
+      <h1>Eu tenho: {people.age}</h1>
+      <h1>eu sou: {people.name}</h1>
+      <img src={people.photo}></img>
+      <h1>INICIO</h1>
+      <button onClick={like}>Like</button>
+      <button onClick={() => deslike()}>deslike</button>
+      {/* <p>eu sou: {profile.bio}</p> */}
+    </div>
+  );
+};
