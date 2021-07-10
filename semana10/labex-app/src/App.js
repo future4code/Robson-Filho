2c538a64836dac1c62b5f8b40e8df0762873f986
Robinndev <robson_dev@hotmail.com>
2021-07-09 22:53:41 -0300

    adicionando router nas pages do labex

--- semana10/labex-app/src/App.js
+++ semana10/labex-app/src/App.js
@@ -1,10 +1,54 @@
-
+import React from "react";
+import { AdminHomePage } from "./pages/AdminHomePage";
+import { AplicationFormPage } from "./pages/ApplicationFormPage";
+import { CreateTripPage } from "./pages/CreateTripPage";
+import { HomePage } from "./pages/HomePage";
+import { ListTripsPage } from "./pages/ListTripsPage";
+import { LoginPage } from "./pages/LoginPage";
+import { TripDetailsPage } from "./pages/TripDetailsPage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ErrorPage } from "./pages/ErrorPage";
 
 function App() {
   return (
-   <div>
-     <h1>Hello world!</h1>
-   </div>
+
+    <BrowserRouter>
+      <Switch>
+
+      <Route exact path={"/"}>
+      <HomePage />
+      </Route>
+
+      <Route exact path={"/admin"}>
+        <AdminHomePage />
+      </Route>
+
+      <Route exact path={"/aplication"}>
+        <AplicationFormPage />
+      </Route>
+
+      <Route exact path={"/trip"}>
+        <CreateTripPage />
+      </Route>
+
+      <Route exact path={"/list"}>
+      <ListTripsPage />
+      </Route>
+
+      <Route exact path={"/subscribe"}>
+        <LoginPage />
+      </Route>
+
+      <Route exact path={"/details"}>
+      <TripDetailsPage />
+      </Route>
+
+      <Route>
+      <ErrorPage/>
+      </Route>
+
+      </Switch>
+  </BrowserRouter>
   );
 }
 
