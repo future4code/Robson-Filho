09026e4173b51255d4885713b9d83b7ab33241c3
robinndev <robson_dev@hotmail.com>
2021-08-10 09:28:50 -0300

    adicionando desafio parte 1

--- semana15/aula41-node/index1/index.js
+++ semana15/aula41-node/index1/index.js
@@ -2,15 +2,26 @@
 
 // b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
 
-// const Nome = "Robson"
-// const idade = Number("19")
-// const tempo = Number("7")
+const Nome = "a"
+const idade = Number("3")
+const tempo = Number("")
 
-// console.log(`"Olá, ${Nome}! Você tem ${idade} anos."`)
+// const printMessager = () => {console.log(`"Olá, ${Nome}! Você tem ${idade} anos."`)}
+// printMessager()
+
+if(Nome && idade != "da"){
+    console.log(`"Olá, ${Nome}! Você tem ${idade} anos.`)
+} else {
+    console.log("Error, falta preencher algum campo")
+}
 
 // c) Altere o programa acima para que mostre também a sua idade daqui a sete anos.
 
-// console.log(`Olá, ${Nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + tempo}`)
+if(Nome && idade && tempo !== "da"){
+    console.log(`Olá, ${Nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + tempo}`)
+} else {
+    console.log("Error, falta preencher algum campo")
+}
 
 // Crie uma aplicação Node que recebe uma string representando uma operação matemática e dois valores numéricos. O retorno deverá ser o resultado da operação selecionada utilizando os 2 valores fornecidos.
 
