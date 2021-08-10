965ed8cb35f3e6234b52a7288fec8d6f138bcfac
robinndev <robson_dev@hotmail.com>
2021-08-09 21:35:16 -0300

    adicionando atividade completa sobre node.

---
+++ semana15/aula41-node/index1/index.js
@@ -0,0 +1,18 @@
+// a)  process.argv[3].
+
+// b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
+
+// const Nome = "Robson"
+// const idade = Number("19")
+// const tempo = Number("7")
+
+// console.log(`"Olá, ${Nome}! Você tem ${idade} anos."`)
+
+// c) Altere o programa acima para que mostre também a sua idade daqui a sete anos.
+
+// console.log(`Olá, ${Nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + tempo}`)
+
+// Crie uma aplicação Node que recebe uma string representando uma operação matemática e dois valores numéricos. O retorno deverá ser o resultado da operação selecionada utilizando os 2 valores fornecidos.
+
+
+
